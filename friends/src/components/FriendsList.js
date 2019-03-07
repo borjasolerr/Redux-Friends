cd064af4a384c61e00553f14dd32820e52ca80b8
borjasolerr <borjasolrey@gmail.com>
2019-03-07 15:31:56 +0100

    Added friend component

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Friend from './Friend';
+
+const FriendsList = props => {
+  return (
+    <div>
+      <ul>
+        {props.friends.map((friend, index) => {
+          return <Friend friend={friend} key={index} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default FriendsList;
