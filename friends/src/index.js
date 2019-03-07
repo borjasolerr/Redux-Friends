94d9249d1739825b517258daf8212464a5eab41c
borjasolerr <borjasolrey@gmail.com>
2019-03-07 15:26:03 +0100

    Added friendsReducer.js and fetch friends

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import mainReducer from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import * as serviceWorker from './serviceWorker';
+const friendStore = createStore(mainReducer, applyMiddleware(thunk, logger));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={friendStore}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
